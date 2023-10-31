632db8387cc9b167827b0e6be1f4afe8d4e5698e
ponjs <ponlin@qq.com>
2023-11-01 01:57:39 +0800

    perf: 配置文档视图

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import StyledComponentsRegistry from '../lib/AntdRegistry'
 import 'tailwindcss/tailwind.css'
 import '../styles/preflight.css'
 import '../styles/global.css'
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,13 +13,16 @@ export const metadata: Metadata = {
   title: 'XNode',
 }
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-CN">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-      />
       <body className={inter.className}>
         <StyledComponentsRegistry>
           <ConfigProvider locale={zhCN}>{children}</ConfigProvider>
