b8cfe67c11ee8b220e9ea53579372de3825798e1
ponjs <ponlin@qq.com>
2023-10-31 16:22:25 +0800

    chore: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/*
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
+    'react/display-name': 'off',
+    'react/self-closing-comp': 'warn',
+  },
+}
