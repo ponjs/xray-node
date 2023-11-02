c200ae601fcc9a9da6ef2580687aa53b93568406
ponjs <ponlin@qq.com>
2023-11-02 23:52:18 +0800

    fix: 处理时间组件国际化

--- src/lib/AntdRegistry.tsx
+++ src/lib/AntdRegistry.tsx
@@ -3,7 +3,11 @@
 import { useMemo } from 'react'
 import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs/lib'
 import { useServerInsertedHTML } from 'next/navigation'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
 import type Entity from '@ant-design/cssinjs/es/Cache'
+
+dayjs.locale('zh-cn')
 
 export default function StyledComponentsRegistry({ children }: React.PropsWithChildren) {
   const cache = useMemo<Entity>(() => createCache(), [])
